8b22d5edaf911e8dc8340fb6711cc873c58510eb
yz56 <yiming.zhang@thoughtworks.com>
2020-07-30 23:33:59 +0800

    add a book to staticBook(libaray)

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -10,12 +10,14 @@ public class BibliotecaApp {
     public static String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     public static List<Book> staticBooks = new ArrayList<Book>();
     static Book book0 = new Book(0,"book0","Tony",1993, true);
-    static Book book1 = new Book(1,"book1","Jenny",1991, false);
+    static Book book1 = new Book(1,"book1","Jenny",1991, true);
     static Book book2 = new Book(2,"book2","Jim",2020, true);
+    static Book book3 = new Book(2,"book3","Bob",1888, false);
     static{
         staticBooks.add(book0);
         staticBooks.add(book1);
         staticBooks.add(book2);
+        staticBooks.add(book3);
     }
 
     public static void main(String[] args) {
